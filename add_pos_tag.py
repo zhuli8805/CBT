3f149a704fb25248e9f63010d5579d89b802bf98
Ducaz <lyx712@gmail.com>
2016-12-03 00:40:26 +0000

    Update add_pos_tag.py

--- add_pos_tag.py
+++ add_pos_tag.py
@@ -4,15 +4,16 @@ Created on Wed Nov 30 23:46:54 2016
 
 @author: Yixuan Li
 """
+import re
+import sys
+import time
+from pycorenlp import StanfordCoreNLP
+from os.path import basename, join, splitext
+from glob import glob
 
 """
 java -mx4g -cp "*" edu.stanford.nlp.pipeline.StanfordCoreNLPServer
 """
-
-import re, sys, time
-from pycorenlp import StanfordCoreNLP
-from os.path import basename, join, splitext
-from glob import glob
 
 missing_word = 'XXXXX'
 corenlp = StanfordCoreNLP('http://localhost:9000')
@@ -20,29 +21,31 @@ corenlp_settings = {'annotators': 'tokenize,ssplit,pos,ner', "outputFormat": "te
 re_nlp_annotate = re.compile('\[Text=(\S+).*PartOfSpeech=(\S+)')
 re_punct = re.compile('^[^a-zA-Z]+$')
 
-pattern = 'cbtest_V*'
-#pattern = 'test*'
+#pattern = 'cbtest_V*'
+pattern = 'test*'
 if len(sys.argv) > 1:
     pattern = sys.argv[1]
-data_path = 'CBTest Datasets\CBTest\data'
+#data_path = 'CBTest Datasets\CBTest\data'
+data_path = 'D:\Yixuan Li\Documents\TUoS\Industrial Team Project\CBTest\CoreNLP_tag_reader'
 
 file_in_list = glob(join(data_path, pattern))
 file_out_list = []
 
 for f_in in file_in_list:
     base = basename(f_in)
-    f_out = join(data_path, splitext(base)[0]+'_WP'+splitext(base)[1])
+    f_out = join(data_path, splitext(base)[0] + '_WP' + splitext(base)[1])
     file_out_list.append(f_out)
-    
+
 
 def countLines(filename):
-    i = 0 
-    file = open(filename,'r')
+    i = 0
+    file = open(filename, 'r')
     for line in file:
         i += 1
     file.close()
     return i
-    
+
+
 def display_files():
     for f_in, f_out in zip(file_in_list, file_out_list):
         print(basename(f_in))
@@ -56,12 +59,11 @@ def glob_CBT_files():
             with open(f_out, 'w') as file_out:
                 print('[Start processing] ' + basename(f_in))
                 read_and_write(file_in, file_out, totalLines)
-    file_in.close()
-    file_out.close()
+
 
 def read_and_write(file_in, file_out, totalLines):
-    initLineNo = totalLines/1000
-    stepLength = totalLines/10
+    initLineNo = totalLines / 1000
+    stepLength = totalLines / 10
     nextLineNo = initLineNo
     iLine = 0
     starttime = time.time()
@@ -71,8 +73,9 @@ def read_and_write(file_in, file_out, totalLines):
         if iLine >= nextLineNo:
             timesofar = (time.time() - starttime) / 60
             totaltime = (timesofar * totalLines / iLine)
-            timeleft = (timesofar * (totalLines-iLine) / iLine)
-            print('[Progress]: %3.2f%% (%d/%d)  %.2f/%.2fmins %.2fmins left' % (iLine/totalLines*100, iLine, totalLines, timesofar, totaltime, timeleft))                
+            timeleft = (timesofar * (totalLines - iLine) / iLine)
+            print('[Progress]: %3.2f%% (%d/%d)  %.2f/%.2fmins %.2fmins left' % (
+            iLine / totalLines * 100, iLine, totalLines, timesofar, totaltime, timeleft))
             if nextLineNo is initLineNo:
                 nextLineNo = stepLength
             else:
@@ -86,6 +89,8 @@ def read_and_write(file_in, file_out, totalLines):
 def get_nlp_tag(text):
     text_list = text.split()
     num = int(text_list.pop(0))
+    candidates = 0
+    answer = 0
     if num == 21:
         candidates = text_list.pop().split('|')
         answer = text_list.pop()
@@ -95,13 +100,16 @@ def get_nlp_tag(text):
     output_list = [str(num)]
     if num == 21:
         content_pair, missing_word_idx = identify_query_tag(content)
-        candidates_pair_dict = identify_target_tag(content, text_list, candidates, missing_word_idx)
-        candidates_pair.append('|'.join([':'.join([x,y]) for x, y in candidates_pair_dict.items()]))
-        answer_pair.append(':'.join([answer,candidates_pair_dict[answer]]))
+        candidates_pair_dict = identify_target_tag(content, candidates, missing_word_idx)
+        candidates_pair.append('|'.join([':'.join([x, y]) for x, y in candidates_pair_dict.items()]))
+        if answer not in candidates_pair_dict:
+            candidates_pair_dict[answer] = 'ERR'
+        answer_pair.append(':'.join([answer, candidates_pair_dict[answer]]))
         output_list += content_pair + answer_pair + candidates_pair
     else:
         output_list.append(identify_tag(content))
     return output_list
+
 
 def identify_tag(content):
     content_pair = []
@@ -114,7 +122,8 @@ def identify_tag(content):
             content_pair.append(':'.join([m.group(1), m.group(2)]))
     content_tag = ' '.join(content_pair)
     return content_tag
-    
+
+
 def identify_query_tag(content):
     content_pair = []
     output = corenlp.annotate(content, properties=corenlp_settings)
@@ -132,11 +141,12 @@ def identify_query_tag(content):
     content_tag = [' '.join(content_pair)]
     return content_tag, missing_word_idx
 
-def identify_target_tag(content, splitted_content, candidates, missing_word_idx):
+
+def identify_target_tag(content, candidates, missing_word_idx):
     target_tag = {}
-    #missing_word_idx = identify_missing_word_pos(splitted_content) + 2
+    # missing_word_idx = identify_missing_word_pos(split_content) + 2
     for word in candidates:
-        output = corenlp.annotate(content.replace(missing_word, word), 
+        output = corenlp.annotate(content.replace(missing_word, word),
                                   properties=corenlp_settings)
         line = output.splitlines()[missing_word_idx]
         m = re_nlp_annotate.match(line)
@@ -144,10 +154,10 @@ def identify_target_tag(content, splitted_content, candidates, missing_word_idx)
             target_tag[m.group(1)] = m.group(2)
     return target_tag
 
-    
-def identify_missing_word_pos(splitted_content):
-    for i in range(len(splitted_content)-1):
-        if splitted_content[i] == missing_word:
+
+def identify_missing_word_pos(split_content):
+    for i in range(len(split_content) - 1):
+        if split_content[i] == missing_word:
             return i
 
 
@@ -158,4 +168,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
